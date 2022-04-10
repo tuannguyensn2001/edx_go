ada319419605b4c8a9c015f07726e2e608461fbf
Tuan Nguyen Van - 19000372 <devpro2001@gmail.com>
2022-04-10 19:28:19 +0700

    .

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 	//dsn := os.Getenv("DATABASE")
 
-	dsn := "admin:java2001@tcp(database-1.ckh8wqhaxqge.us-east-1.rds.amazonaws.com:3306)/edx?"
+	dsn := "admin:java2001@tcp(database-1.ckh8wqhaxqge.us-east-1.rds.amazonaws.com:3306)/edx?charset=utf8mb4&parseTime=True&loc=Local"
 
 	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
