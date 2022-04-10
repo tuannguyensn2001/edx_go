a2afd91a701574fc15e0e2de2a612ebe3e37be42
Tuan Nguyen Van - 19000372 <devpro2001@gmail.com>
2022-04-10 18:45:07 +0700

    .

--- main.go
+++ main.go
@@ -1,76 +1,54 @@
-//package main
-//
-//import (
-//	app_ctx "edx_go/component"
-//	"edx_go/middleware"
-//	"edx_go/routes"
-//	"edx_go/socket"
-//	"github.com/gin-gonic/gin"
-//	"github.com/joho/godotenv"
-//	"gorm.io/driver/mysql"
-//	"gorm.io/gorm"
-//	"log"
-//	"os"
-//)
-//
-//func main() {
-//
-//	if err := godotenv.Load(); err != nil {
-//		log.Fatalln("env loaded err")
-//	}
-//
-//	dsn := os.Getenv("DATABASE")
-//
-//	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-//
-//	if err != nil {
-//		log.Fatalln("DB connect failed", err)
-//	}
-//
-//	appCtx := app_ctx.NewAppContext(db)
-//
-//	r := gin.Default()
-//
-//	r.Use(middleware.CORSMiddleware())
-//
-//	r.Use(middleware.Recover(appCtx))
-//
-//	r.StaticFile("demo", "./demo.html")
-//
-//	routes.DeclareRoute(r, appCtx)
-//
-//	engine := socket.NewEngine()
-//
-//	err = engine.Run(appCtx, r)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	port := os.Getenv("PORT")
-//
-//	err = r.Run(":" + port)
-//	if err != nil {
-//		return
-//	}
-//}
 package main
 
 import (
+	app_ctx "edx_go/component"
+	"edx_go/middleware"
+	"edx_go/socket"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 	"os"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln("env loaded err")
+	}
+
+	dsn := os.Getenv("DATABASE")
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln("DB connect failed", err)
+	}
+
+	appCtx := app_ctx.NewAppContext(db)
+
 	r := gin.Default()
 
-	r.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "done",
-		})
-	})
+	r.Use(middleware.CORSMiddleware())
+
+	r.Use(middleware.Recover(appCtx))
+
+	r.StaticFile("demo", "./demo.html")
+
+	//routes.DeclareRoute(r, appCtx)
+
+	engine := socket.NewEngine()
+
+	err = engine.Run(appCtx, r)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	port := os.Getenv("PORT")
 
-	r.Run(":" + port)
+	err = r.Run(":" + port)
+	if err != nil {
+		return
+	}
 }
