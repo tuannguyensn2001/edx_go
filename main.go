c38481c2b7657a857850e9d52ef3677fe7c399bc
Tuan Nguyen Van - 19000372 <devpro2001@gmail.com>
2022-04-10 20:48:35 +0700

    update

--- main.go
+++ main.go
@@ -1,50 +1,56 @@
 package main
 
 import (
+	app_ctx "edx_go/component"
+	"edx_go/middleware"
+	"edx_go/socket"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 func main() {
 
-	//if err := godotenv.Load(); err != nil {
-	//	log.Fatalln("env loaded err")
-	//}
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln("env loaded err")
+	}
 
 	//dsn := os.Getenv("DATABASE")
 
 	dsn := "admin:java2001@tcp(database-1.ckh8wqhaxqge.us-east-1.rds.amazonaws.com:3306)/edx?charset=utf8mb4&parseTime=True&loc=Local"
 
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalln("DB connect failed", err)
 	}
 	//
-	//appCtx := app_ctx.NewAppContext(db)
-	////
-	//r := gin.Default()
+	appCtx := app_ctx.NewAppContext(db)
 	//
-	//r.Use(middleware.CORSMiddleware())
-	//
-	//r.Use(middleware.Recover(appCtx))
-	//
-	//r.StaticFile("demo", "./demo.html")
-	//
-	////routes.DeclareRoute(r, appCtx)
-	//
-	//engine := socket.NewEngine()
-	//
-	//err = engine.Run(appCtx, r)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//port := os.Getenv("PORT")
-	//
-	//err = r.Run(":" + port)
-	//if err != nil {
-	//	return
-	//}
+	r := gin.Default()
+
+	r.Use(middleware.CORSMiddleware())
+
+	r.Use(middleware.Recover(appCtx))
+
+	r.StaticFile("demo", "./demo.html")
+
+	//routes.DeclareRoute(r, appCtx)
+
+	engine := socket.NewEngine()
+
+	err = engine.Run(appCtx, r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port := os.Getenv("PORT")
+
+	err = r.Run(":" + port)
+	if err != nil {
+		return
+	}
 }
